841950c6e0052849bbb6257f28770092934f9d37
Swastik19Nit <122457803+Swastik19Nit@users.noreply.github.com>
2025-02-14 13:14:06 +0530

    Added chunk size warning limit increase

---
+++ frontend/dist/assets/react-vendor-2b4d0748.js
@@ -0,0 +1,1 @@
+import"./three-vendor-38ebdcda.js";
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,23 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
+    chunkSizeWarningLimit: 1600,
     rollupOptions: {
-      external: ['react-router-dom'], // Add react-router-dom to external
+      external: ['react-router-dom'],
+      output: {
+        manualChunks: {
+          'three-vendor': ['three', '@react-three/fiber', '@react-three/drei'],
+          'react-vendor': ['react', 'react-dom'],
+        }
+      }
     },
+    optimizeDeps: {
+      include: ['three', '@react-three/fiber', '@react-three/drei']
+    }
   },
+  assetsInclude: ['**/*.gltf', '**/*.glb', '**/*.hdr', '**/*.fbx'],
 })
